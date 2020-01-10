2e4ffc87d0ce090b5ffde6deaeb3c3bb39c9a0e3
cohensharon <cohensharon1995@gmail.com>
2020-01-10 12:29:39 -0800

    templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express')
+const hbs = require('hbs');
 
 
 const app = express()
@@ -10,11 +11,13 @@ const app = express()
 
 //define paths for express config
 const staticPublicPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //setup hbs engine and views location
 app.set('views', viewsPath);
 app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
 
 //set up static dir to serve
 app.use(express.static(staticPublicPath));
@@ -28,7 +31,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About',
+        title: 'About me',
         name: 'Sharon',
     });
 });
@@ -36,6 +39,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         message: 'Help message',
+        title: 'Help',
+        name: 'Sharon',
     });
 });
 
